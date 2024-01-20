d7c66bb3f1d1919cc427b971a27702e7a7d23e1d
Carlos Lapao <cjlapao@gmail.com>
2024-01-20 06:44:39 +0000

    adding initial structure for the templater

---
+++ src/interfaces/provisioner.go
@@ -0,0 +1,14 @@
+package interfaces
+
+import (
+	provisioner_context "github.com/cjlapao/github-templater/context"
+)
+
+type Provisioner interface {
+	New(ctx provisioner_context.ProvisionerContext, config ProvisionerConfig) Provisioner
+	Name() string
+	ID() string
+	Context() provisioner_context.ProvisionerContext
+	Provision() error
+	Languages() []string
+}
---
+++ src/provisioners/makefile/main.go
@@ -0,0 +1,59 @@
+package makefile
+
+import (
+	"time"
+
+	"github.com/cjlapao/github-templater/constants"
+	"github.com/cjlapao/github-templater/context"
+	"github.com/cjlapao/github-templater/interfaces"
+)
+
+type MakeFileProvisioner struct {
+	id        string
+	name      string
+	languages []string
+	ctx       context.ProvisionerContext
+	config    interfaces.ProvisionerConfig
+}
+
+func New(ctx context.ProvisionerContext, config interfaces.ProvisionerConfig) *MakeFileProvisioner {
+	result := &MakeFileProvisioner{
+		id:        "31264948-5ec7-4d7f-abe3-fc7ec7f70344",
+		name:      "makefile",
+		ctx:       ctx,
+		languages: []string{"all"},
+		config:    config,
+	}
+
+	result.ctx.WithValue(constants.ContextResourceName, result.name)
+
+	return result
+}
+
+func (p MakeFileProvisioner) New(ctx context.ProvisionerContext, config interfaces.ProvisionerConfig) interfaces.Provisioner {
+	item := New(ctx, config)
+	return item
+}
+
+func (p MakeFileProvisioner) Name() string {
+	return p.name
+}
+
+func (p MakeFileProvisioner) ID() string {
+	return p.id
+}
+
+func (p MakeFileProvisioner) Context() context.ProvisionerContext {
+	return p.ctx
+}
+
+func (p MakeFileProvisioner) Languages() []string {
+	return p.languages
+}
+
+func (p MakeFileProvisioner) Provision() error {
+	p.ctx.LogInfo("Provisioning Makefile")
+	time.Sleep(5 * time.Second)
+	p.ctx.LogInfo("Makefile provisioned")
+	return nil
+}
